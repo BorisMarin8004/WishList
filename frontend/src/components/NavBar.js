d10e5c427c4025067f66d766fbf522d7ddc1ba31
Emma <71865150+Raisalis@users.noreply.github.com>
2021-10-06 21:33:18 -0700

    Home page framework made. NavBar navigation framework created.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,7 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 const NavBar = () => {
+    const btnLogoClick = () => {
+        console.log("logo clicked.")
+    }
+
+    const btnWishlistClick = () => {
+        console.log("wishlist clicked.")
+    }
+
+    const btnAddItemClick = () => {
+        console.log("add item clicked.")
+    }
+
+    const btnProfileClick = () => {
+        console.log("profile clicked.")
+    }
+
+    const btnLogoutClick = () => {
+        console.log("logout button clicked.")
+    }
     return (
         <div>
-            
+            <button>Logo Placeholder</button>
+            <button>Wishlist Placeholder</button>
+            <button>Add Item Placeholder</button>
+            <button>Profile Placeholder</button>
+            <button>Logout Placeholder</button>
         </div>
     )
 }
