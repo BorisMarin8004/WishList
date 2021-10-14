4d56e3e721b381c907442520df906176b2e17aee
Emma <71865150+Raisalis@users.noreply.github.com>
2021-10-13 17:40:57 -0700

    Added CSS for all currently made pages/components.

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -7,8 +7,16 @@ const defonClick = () => {
 
 // TODO: change the style changes to be css based
 const Button = ({ text, color, onClick }) => {
+    let rColor = "";
+    if(color == "green") {
+        rColor="#8EE167";
+    } else if(color == "orange") {
+        rColor = "#FFAF83";
+    } else {
+        rColor = "#ED5F5F";
+    }
     return (
-        <button onClick={onClick} style={{ color: color}} className='btn'>
+        <button onClick={onClick} style={{ color: rColor, backgroundColor: "white", borderRadius: "14px", boxShadow: "0px 4px 0px" + rColor, width: "478px", height: "60px", fontSize: "24px", border: "none"}} className='btn'>
             {text}
         </button>
     )
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '../css/NavBar.css'
 
 const NavBar = () => {
     const btnLogoClick = () => {
