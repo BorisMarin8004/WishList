2749307287164561ffa8dec356339dfb37987f32
BorisMarin8004 <bmar8004@gmail.com>
2021-10-13 22:46:35 -0700

    User registration.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,24 +1,38 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from "axios";
-import {getLoginConfig} from "../network/RequestTemples";
+import {getLoginConfig, getSignUpConfig} from "../network/RequestTemples";
+import Button from "../components/Button";
 
 export default function Login({ setToken }) {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        console.log({"username": username, "password": password})
-        const token = (await axios(getLoginConfig({"username": username, "password": password}))).data.token
-        console.log(token)
-        setToken(token)
+    function handleLogin() {
+        axios(getLoginConfig({"username": username, "password": password})).then(
+            res => setToken(res.data.token)
+        ).catch(
+            err => {
+                alert("Incorrect Username or Password")
+                console.log(err)
+            }
+        )
+    }
+
+    function handleSignUp() {
+        axios(getSignUpConfig({"username": username, "password": password})).then(
+            res => alert(`User ${res.data.username} registered`)
+        ).catch(
+            err => {
+                alert("Registration failed")
+                console.log(err)
+            }
+        )
     }
 
     return(
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-            <form onSubmit={handleSubmit}>
             <label>
                 <p>Username</p>
                 <input type="text" onChange={e => setUserName(e.target.value)} />
@@ -28,9 +42,9 @@ export default function Login({ setToken }) {
                 <input type="password" onChange={e => setPassword(e.target.value)} />
             </label>
             <div>
-                <button type="submit">Submit</button>
+                <Button text="Login" color="green" onClick={handleLogin}/>
+                <Button text="Sign Up" color="green" onClick={handleSignUp}/>
             </div>
-            </form>
         </div>
     )
 }
