e100738443b405f1d1c675db79397d229ec3345c
BorisMarin8004 <bmar8004@gmail.com>
2021-10-16 11:07:42 -0700

    Moved ports.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,18 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-jd67-812eb7h^n@3r6$ak+jnvk)shj8b0fnjc1*you1r1^kf4*'
-
+PORT = "3000"
 ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://0.0.0.0:3000",
+    "http://localhost:{}".format(PORT),
+    "http://0.0.0.0:{}".format(PORT),
 ]
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
-    "http://localhost:3000",
-    "http://0.0.0.0:3000",
+    "http://localhost:{}".format(PORT),
+    "http://0.0.0.0:{}".format(PORT),
 )
 
 CSRF_COOKIE_NAME = "XSRF-TOKEN"
