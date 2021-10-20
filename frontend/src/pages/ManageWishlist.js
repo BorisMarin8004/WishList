3274c83392f8a3fb41ac2ce8233be27230cfd9eb
Emma <71865150+Raisalis@users.noreply.github.com>
2021-10-20 12:29:19 -0700

    ManageWishlist page css done. Item component created.

--- frontend/src/pages/ManageWishlist.js
+++ frontend/src/pages/ManageWishlist.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Button from '../components/Button'
+import '../css/ManageWishlist.css'
+import Item from '../components/Item'
 
 
 export default function ManageWishlist() {
+    // TODO: Handles deletion of an item
+    function handleDelete() {
+        console.log("Delete clicked")
+    }
+    // TODO: Get wishlist based on user details
+    
     return (
-        <h2>Empty ManageWishlist</h2>
+        <main>
+            <div id="wishlistpage">
+                <h1>My Wishlist</h1>
+                {/* For each item in wishlist, add a new container*/}
+                <div className="items">
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                    <Item />
+                </div>
+            </div>
+            <div className="background-cover-home">
+            </div>
+        </main>
     )
 }
