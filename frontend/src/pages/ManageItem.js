20247155eaaebd7520d1b8cbbdc16a613a6d1eb6
michaelhaidar <mhaidar@csumb.edu>
2021-10-21 14:59:32 -0700

    non working manageitems.js

--- frontend/src/pages/ManageItem.js
+++ frontend/src/pages/ManageItem.js
@@ -1,35 +1,63 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Button from '../components/Button'
-function AddItem() {
+import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
+import axios from "axios";
+import {getLoginConfig, getSignUpConfig, getUserModelConfig} from "../network/RequestTemples";
+import Button from "../components/Button";
+import AccountHeader from '../components/AccountHeader'
+import '../css/pages/Login.css'
+import { useUsername, usePassword, useId } from "../customHooks/auth";
 
-	const {token, setToken} = useToken();
+/*  **Todo:
+    add and remove item, to wishlist, user had access to item if its in their wishlist
+    create empty wishlist and add the item
+    add item will just add item to wishlist and stay on the page.
+    have access to all wishlists.
+    containers: Item Name, price, url, picture url, wishlist
+*/
 
-	function addItem() {
-		axios(getAddItemConfig({"description":description, "price":price, "URL":URL,  })).then(
-            res => setToken(res.data.token)
-        ).catch(
-            err => {
-                console.log(err)
-            }
-        )
-    }
+export default function Login({ ID, Token}) {
+    const { inputUsername, setInputUsername } = useState();
+    const { inputPassword, setInputPassword } = useState();
 
     return(
         <div>
-            <h1 class="title">Add an Item </h1>
-            <div class ="container">
-				<input type="text" value={description} placeholder="Description" class="editText"></input>
-				<br></br>
-				<input type="text" value={price} placeholder ="Price" class="editText"></input>
-				<br></br>
-				<input type="text" value={URL} placeholder="URL"class="editText"></input>
-				<br></br>
-				<button text="Add Item" onClick= {addItem}></button>
-				<br></br>
+            <AccountHeader text='Manage Items' />
+            <div className="container">
+                <div className="entryBox">
+                    <label>ItemName:</label>
+                    <input
+                        type='text'
+                        placeholder='Enter the name of your item.'
+                        value={inputItemname}
+                        onChange={(e) => setInputUsername(e.target.value)}
+                    />
+                </div>
+                <div className="pad"/>
+                <div className="entryBox">
+                    <label>Password:</label>
+                    <input
+                        type='password'
+                        placeholder='Enter Password'
+                        value={inputPassword}
+                        onChange={(e) => setInputPassword(e.target.value)}
+                    />
+                </div>
+                <div className="pad"/>
+                <div className="buttons">
+                    <Button text="Login" color="green" onClick={handleLogin}/>
+                    <div className="pad"/>
+                    <Button text="Sign Up" color="green" onClick={handleSignUp}/>
+                </div>
+            </div>
+            <div className="background-cover">
             </div>
         </div>
     )
 }
 
-export default UserProfile
+Login.propTypes = {
+    setId: PropTypes.func.isRequired,
+    setToken: PropTypes.func.isRequired,
+    setUsername: PropTypes.func.isRequired,
+    setPassword: PropTypes.func.isRequired
+};
