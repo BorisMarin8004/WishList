d16d6ecfbe57d050d20645d0e50e0be121ece983
Emma <71865150+Raisalis@users.noreply.github.com>
2021-10-21 10:49:29 -0700

    Added loop functionality for showing wishlist items.

--- frontend/src/pages/ManageWishlist.js
+++ frontend/src/pages/ManageWishlist.js
@@ -7,23 +7,22 @@ import Item from '../components/Item'
 export default function ManageWishlist() {
     // TODO: Handles deletion of an item
     function handleDelete() {
+        alert("Are you sure you want to delete this item?")
         console.log("Delete clicked")
     }
     // TODO: Get wishlist based on user details
-    
+    var i;
+    const items = [];
+    for(i = 0; i < 7; i++) {
+        items.push(<Item onClick={() => {handleDelete()}}/>);
+    }
     return (
         <main>
             <div id="wishlistpage">
                 <h1>My Wishlist</h1>
                 {/* For each item in wishlist, add a new container*/}
                 <div className="items">
-                    <Item />
-                    <Item />
-                    <Item />
-                    <Item />
-                    <Item />
-                    <Item />
-                    <Item />
+                    {items}
                 </div>
             </div>
             <div className="background-cover-home">
