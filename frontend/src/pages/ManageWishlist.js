fccd80f47e30c9877eed6c4e6787ef545b547658
Emma <71865150+Raisalis@users.noreply.github.com>
2021-10-22 14:20:10 -0700

    Delete Button can now be clicked.

--- frontend/src/pages/ManageWishlist.js
+++ frontend/src/pages/ManageWishlist.js
@@ -4,23 +4,39 @@ import '../css/ManageWishlist.css'
 import Item from '../components/Item'
 
 
-export default function ManageWishlist() {
+export default function ManageWishlist(userContext) {
     // TODO: Handles deletion of an item
     function handleDelete() {
-        alert("Are you sure you want to delete this item?")
         console.log("Delete clicked")
+        // Use itemid to delete Item from Wishlist
+        // Reload Wishlist
+        
     }
     // TODO: Get wishlist based on user details
-    var i;
+    /*
+    axios(getWishlistModelConfig("get", token, {"user_id":userContext.})).then(
+        res => {
+            // console.log(userContext)
+            setId(res.data[0].id)
+            setUsername(inputUsername)
+            setPassword(inputPassword)
+        }
+    ).catch(
+        err => {
+            console.log(err)
+        }
+    )
+    */
+    let i = 0;
     const items = [];
     for(i = 0; i < 7; i++) {
-        items.push(<Item onClick={() => {handleDelete()}}/>);
+        items.push(<Item onclick={() => {handleDelete()}}/>);
     }
     return (
         <main>
             <div id="wishlistpage">
                 <h1>My Wishlist</h1>
-                {/* For each item in wishlist, add a new container*/}
+                {/* For each item in wishlist, add a new container */}
                 <div className="items">
                     {items}
                 </div>
